65f701ce04e2b771ba5733e44a75712099089972
semantic-void <90981866+semantic-void@users.noreply.github.com>
2022-10-28 22:50:18 +0530

    Delete assignment directory

--- assignment/src/main/java/com/springmvc/boot/dao/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springmvc.boot.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springmvc.boot.model.UserTable;
-
-public interface UserRepository extends JpaRepository<UserTable, Integer> {
-
-}
